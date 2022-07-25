9e55b913517a85c8f65182f0926a8120ae09b779
wadernik <zakaraya.ilia@gmail.com>
2022-07-25 11:46:12 +0300

    main: added temporary view endpoint for testing purposes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+// TODO: remove this
+Route::get('/orders_export_test', [TestController::class, 'index']);
