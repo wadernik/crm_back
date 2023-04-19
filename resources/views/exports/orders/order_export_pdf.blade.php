e92f8b671e225697cbc06f45970977acc28bbbd6
Alexander Stolyarov <kiberkun@yandex.ru>
2023-04-19 19:44:35 +0300

    fix

--- resources/views/exports/orders/order_export_pdf.blade.php
+++ resources/views/exports/orders/order_export_pdf.blade.php
@@ -8,180 +8,113 @@
         font: normal 9px "DejaVu Sans", sans-serif;
       }
 
-      .border {
-        border: 1px solid black;
-      }
-
-      .thead {
-        background-color: #F9FAFB;
-      }
-
-      .th {
-        font-size: 9px;
-      }
-
-      .td {
-        font-size: 9px;
-      }
-
-      .container {
-        width: 100%
-      }
-
-      @media (min-width: 640px) {
-        .container {
-          max-width: 640px
-        }
-      }
-
-      @media (min-width: 768px) {
-        .container {
-          max-width: 768px
-        }
-      }
-
-      @media (min-width: 1024px) {
-        .container {
-          max-width: 1024px
-        }
-      }
-
-      @media (min-width: 1280px) {
-        .container {
-          max-width: 1280px
-        }
-      }
-
-      @media (min-width: 1536px) {
-        .container {
-          max-width: 1536px
-        }
-      }
-
-      .mx-auto {
-        margin-left: auto;
-        margin-right: auto
-      }
-
       .table {
-        display: table
+        display: table;
       }
 
       .w-full {
-        width: 100%
-      }
-
-      .w-20 {
-        width: 5rem
+        width: 100%;
       }
 
       .border-collapse {
-        border-collapse: collapse
+        border-collapse: collapse;
+      }
+
+      .border-separate {
+        border-collapse: separate;
+      }
+
+      .border-spacing-2 {
+        border-spacing: 0.5rem 0.5rem;
       }
 
       .border {
-        border-width: 1px
+        border-width: 1px;
       }
 
-      .py-2 {
-        padding-top: 0.5rem;
-        padding-bottom: 0.5rem
+      .px-2 {
+        padding-left: 0.5rem;
+        padding-right: 0.5rem;
       }
 
-      .px-4 {
-        padding-left: 1rem;
-        padding-right: 1rem
-      }
-
-      .text-left {
-        text-align: left
+      .py-1 {
+        padding-top: 0.25rem;
+        padding-bottom: 0.25rem;
       }
 
       .font-bold {
-        font-weight: 700
-      }
-
-      .font-normal {
-        font-weight: 400
+        font-weight: 700;
       }
 
       .text-red-500 {
         --tw-text-opacity: 1;
-        color: rgb(239 68 68 / var(--tw-text-opacity))
-      }
-
-      img {
-          max-width: 100%;
+        color: rgb(239 68 68 / var(--tw-text-opacity));
       }
     </style>
   </head>
   <body>
     @foreach ($orders as $order)
-    <div class="container mx-auto">
-      <table class="w-full border-collapse">
-        <tbody>
-          <tr>
-            <td class="td w-20 py-2 px-4">Улица</td>
-            <td class="td py-2 px-4 font-bold text-red-500">{{ $order['seller']['address'] }}</td>
-          </tr>
-          <tr>
-            <td class="td w-20 py-2 px-4">Код</td>
-            <td class="td py-2 px-4">{{ $order['number'] }}</td>
-          </tr>
-        </tbody>
-      </table>
-      <table class="w-full border-collapse border">
-        <thead class="thead">
-          <tr>
-            <th class="th border py-2 px-4 text-left font-normal">Наименование продукции</th>
-            <th class="th border py-2 px-4 text-left font-normal">Количество</th>
-            <th class="th border py-2 px-4 text-left font-normal">Дата принятия</th>
-            <th class="th border py-2 px-4 text-left font-normal">Дата исполнения</th>
-            <th class="th border py-2 px-4 text-left font-normal">Время отдачи</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td class="td border py-2 px-4">{{ $order['name'] }}</td>
-            <td class="td border py-2 px-4">{{ $order['amount'] }}</td>
-            <td class="td border py-2 px-4">{{ \Carbon\Carbon::parse($order['accepted_date'])->format('d.m.Y') }}</td>
-            <td class="td border py-2 px-4">{{ \Carbon\Carbon::parse($order['order_date'])->format('d.m.Y') }}</td>
-            <td class="td border py-2 px-4">{{ $order['order_time'] }}</td>
-          </tr>
-        </tbody>
-      </table>
-      <table class="w-full border-collapse">
-        <tbody>
-          <tr>
-            <td class="td w-20 py-2 px-4">Оформление</td>
-            <td class="td py-2 px-4">{{ $order['decoration'] ?: '-' }}</td>
-          </tr>
-          <tr>
-            <td class="td w-20 py-2 px-4">Надпись</td>
-            <td class="td py-2 px-4">{{ $order['label'] }}</td>
-          </tr>
-          <tr>
-            <td class="td w-20 py-2 px-4">Комментарий от покупателя</td>
-            <td class="td py-2 px-4">{{ $order['comment'] }}</td>
-          </tr>
-          <tr>
-            <td class="td w-20 py-2 px-4">Код для получения</td>
-            <td class="td py-2 px-4">{{ $order['number'] }}</td>
-          </tr>
-        </tbody>
-      </table>
-      <table class="w-full border-collapse">
-        <tbody>
-          @foreach ($order['files'] as $image)
-          <tr>
-            <td>
-              <img src="{{ $image['url'] }}" />
-            </td>
-          </tr>
-          @endforeach
-        </tbody>
-      </table>
-    </div>
+    <table class="w-full border-separate border-spacing-2 text-[9px]">
+      <tbody>
+        <tr>
+          <td class="w-1/2">
+            <table class="w-full border-collapse">
+              <tbody>
+                <tr>
+                  <td class="border px-2 py-1 font-bold">Улица</td>
+                  <td class="border px-2 py-1 font-bold text-red-500">{{ $order['seller']['address'] }}</td>
+                </tr>
+                <tr>
+                  <td class="border px-2 py-1 font-bold">Код</td>
+                  <td class="border px-2 py-1">{{ $order['number'] }}</td>
+                </tr>
+                <tr>
+                  <td class="border px-2 py-1 font-bold">Наименование продукции</td>
+                  <td class="border px-2 py-1">{{ $order['name'] }}</td>
+                </tr>
+                <tr>
+                  <td class="border px-2 py-1 font-bold">Количество</td>
+                  <td class="border px-2 py-1">{{ $order['amount'] }}</td>
+                </tr>
+                <tr>
+                  <td class="border px-2 py-1 font-bold">Дата принятия</td>
+                  <td class="border px-2 py-1">{{ \Carbon\Carbon::parse($order['accepted_date'])->format('d.m.Y') }}</td>
+                </tr>
+                <tr>
+                  <td class="border px-2 py-1 font-bold">Дата исполнения</td>
+                  <td class="border px-2 py-1">{{ \Carbon\Carbon::parse($order['order_date'])->format('d.m.Y') }}</td>
+                </tr>
+                <tr>
+                  <td class="border px-2 py-1 font-bold">Время отдачи</td>
+                  <td class="border px-2 py-1">{{ $order['order_time'] }}</td>
+                </tr>
+                <tr>
+                  <td class="border px-2 py-1 font-bold">Оформление</td>
+                  <td class="border px-2 py-1">{{ $order['decoration'] ?: '-' }}</td>
+                </tr>
+                <tr>
+                  <td class="border px-2 py-1 font-bold">Надпись</td>
+                  <td class="border px-2 py-1">{{ $order['label'] }}</td>
+                </tr>
+                <tr>
+                  <td class="border px-2 py-1 font-bold">Комментарий от покупателя</td>
+                  <td class="border px-2 py-1">{{ $order['comment'] }}</td>
+                </tr>
+                <tr>
+                  <td class="border px-2 py-1 font-bold">Код для получения</td>
+                  <td class="border px-2 py-1">{{ $order['number'] }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+          <td class="w-1/2">
+            @foreach ($order['files'] as $image)
+            <img src="{{ $image['url'] }}" class="w-1/2" />
+            @endforeach
+          </td>
+        </tr>
+      </tbody>
+    </table>
     @if (!$loop->last)
     <div class="page-break"></div>
     @endif @endforeach
