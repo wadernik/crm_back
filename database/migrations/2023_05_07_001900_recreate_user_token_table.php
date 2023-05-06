f8cd121ffe8ba48297894fe5a1cc54bf3a93d8c7
ilia.zakaraya <zakaraya.ilia@gmail.com>
2023-05-07 00:21:30 +0300

    main: test fix

---
+++ database/migrations/2023_05_07_001900_recreate_user_token_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_token', static function (Blueprint $table) {
+            $table->dropUnique('user_token_user_id_unique');
+        });
+
+        Schema::dropIfExists('user_token');
+
+        Schema::create('user_token', static function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('token');
+
+            $table->unique('user_id');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_token', static function (Blueprint $table) {
+            $table->dropUnique('user_token_user_id_unique');
+        });
+
+        Schema::dropIfExists('user_token');
+
+        Schema::create('user_token', static function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->string('token');
+
+            $table->unique('user_id');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+};
