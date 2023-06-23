2c0cc648ee872fd3d503e0d83f69df572e5b4a9c
ilia.zakaraya <zakaraya.ilia@gmail.com>
2023-06-23 20:25:50 +0300

    v2: fix for role manager create and update methods

--- app/Managers/Role/RoleManager.php
+++ app/Managers/Role/RoleManager.php
@@ -7,22 +7,45 @@ namespace App\Managers\Role;
 use App\DTOs\Role\CreateRoleDTOInterface;
 use App\DTOs\Role\UpdateRoleDTOInterface;
 use App\Models\Role\Role;
+use App\Models\Role\RoleInterface;
 use Illuminate\Database\Eloquent\Model;
 
 final class RoleManager implements RoleManagerInterface
 {
     public function create(CreateRoleDTOInterface $roleDTO): Model
     {
-        return Role::query()->create($roleDTO->toArray());
+        /** @var RoleInterface|Model $role */
+        $role = Role::query()->create(array_filter([
+            'name' => $roleDTO->name(),
+            'label' => $roleDTO->label(),
+        ]));
+
+        if ($roleDTO->permissions()) {
+            $permissions = collect($roleDTO->permissions())->pluck('id')->toArray();
+
+            $role->permissions()->sync($permissions);
+        }
+
+        return $role;
     }
 
     public function update(int $id, UpdateRoleDTOInterface $roleDTO): ?Model
     {
+        /** @var RoleInterface|Model $role */
         if (!$role = Role::query()->find($id)) {
             return null;
         }
 
-        $role->update($roleDTO->toArray());
+        $role->update(array_filter([
+            'name' => $roleDTO->name(),
+            'label' => $roleDTO->label()
+        ]));
+
+        if ($roleDTO->permissions()) {
+            $permissions = collect($roleDTO->permissions())->pluck('id')->toArray();
+
+            $role->permissions()->sync($permissions);
+        }
 
         return $role;
     }
